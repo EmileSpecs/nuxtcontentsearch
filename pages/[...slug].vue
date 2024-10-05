e5955856fe0e8cfa6e6c73e489c94817487dfd54
EmileSpecs <emile.sievers@gmail.com>
2024-10-05 19:43:54 +0200

    Init

---
+++ pages/[...slug].vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <ContentDoc />
+
+    <input v-model="search">
+
+    <pre>{{ results }} </pre>
+  </main>
+</template>
+
+<script lang="ts" setup>
+const search  = ref('')
+
+const results = await searchContent(search)
+</script>
